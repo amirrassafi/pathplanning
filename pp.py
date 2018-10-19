b580e85551042d9ab56742c175bf3c9de38363be
amir rassafi <amir.rassafi@gmail.com>
2018-10-19 23:25:24 +0330

    pyqt plot obstacles

--- pp.py
+++ pp.py
@@ -1,10 +1,45 @@
-import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui.pp_ui import Ui_MainWindow
+from PyQt5 import QtWidgets
+import sys
+import math
+from shapely.geometry import Polygon
+from descartes import PolygonPatch
 
-app = QApplication(sys.argv)
-mw = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(mw)
-mw.show()
-sys.exit(app.exec_())
+class Obstacle(Polygon):
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+        def __add__(self, other):
+            return (self.x + other.x, self.y + other.y)
+
+    def __init__(self, center_point, size):
+        super().__init__()
+        self.center = center_point
+        corners = [ self.Point(-1, -1), self.Point(-1, 1), self.Point(1, 1), self.Point(1, -1) ]
+        new_corners = [ c+center_point for c in corners]
+        self.p = Polygon(new_corners)
+
+    def getDrawble(self, color):
+        return PolygonPatch(self.p, color=color)
+#Ui class
+class Ui(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+# Create GUI application
+app = QtWidgets.QApplication(sys.argv)
+form = Ui()
+for i in range(10):
+    form.widget.canvas.ax.add_patch(Obstacle(Obstacle.Point(i,i), 1).getDrawble("red"))
+form.widget.canvas.ax.autoscale(enable=True, axis='both', tight=None)
+form.widget.canvas.ax.grid(b=None, which='both', axis='both')
+form.show()
+app.exec_()
+
+
+
+
+
--- ui/pp_ui.py
+++ ui/pp_ui.py
@@ -11,32 +11,32 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 300)
-        self.centralWidget = QtWidgets.QWidget(MainWindow)
-        self.centralWidget.setObjectName("centralWidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton.setGeometry(QtCore.QRect(150, 190, 89, 25))
+        MainWindow.resize(744, 543)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.widget = MplWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(10, 10, 541, 401))
+        self.widget.setObjectName("widget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(600, 30, 97, 27))
         self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralWidget)
-        self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 400, 22))
-        self.menuBar.setObjectName("menuBar")
-        MainWindow.setMenuBar(self.menuBar)
-        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
-        self.mainToolBar.setObjectName("mainToolBar")
-        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
-        self.statusBar = QtWidgets.QStatusBar(MainWindow)
-        self.statusBar.setObjectName("statusBar")
-        MainWindow.setStatusBar(self.statusBar)
-
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 744, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "pathplanning"))
+        self.pushButton.setText(_translate("MainWindow", "reset"))
 
+from mplwidget import MplWidget
 
 if __name__ == "__main__":
     import sys
@@ -44,6 +44,8 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    ui.widget.canvas.ax.plot(range(10),range(10))
+    ui.widget.canvas.draw()
     MainWindow.show()
     sys.exit(app.exec_())
 
